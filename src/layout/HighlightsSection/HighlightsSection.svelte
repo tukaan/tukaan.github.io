56a729ee83db38a969b1202e421ec9859e0bedcd
rdbende <rdbende@gmail.com>
2022-12-23 01:14:14 +0100

    Oh shitt

--- src/layout/HighlightsSection/HighlightsSection.svelte
+++ src/layout/HighlightsSection/HighlightsSection.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <PageSection id="highlight-section">
-	<h2>Powerful. Innovative.<br>Intuitive.</h2>
+	<h2>Powerful. Modern.<br>Intuitive.</h2>
 	<p style="max-width: 80ch;">
 		With Tukaan you can create amazing GUI apps with ease. Tukaan is intuitive and powerful - the complete opposite of Tkinter, although it was initially designed as an alternative to it.
 	</p>
